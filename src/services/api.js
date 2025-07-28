e854778d600005c45fb43ec08b681d8f8f752b42
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-07-29 00:44:38 +0530

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://micandmind-backend.onrender.com",
 });
 
 // Add token automatically
